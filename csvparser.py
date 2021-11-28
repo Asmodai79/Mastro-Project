e4c986e318299a140ea2894e0148218300f0caa9
Michele <michele.marini@outlook.com>
2021-11-28 18:54:12 +0100

    aggiunti file per test

--- csvparser.py
+++ csvparser.py
@@ -2,6 +2,15 @@ import csv
 
 def readcsvFile(filename):
     with open(filename, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar=',')
         for row in spamreader:
             print(', '.join(row))
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            try:
+                print("file di test semplice " + row['organization'], row['alone'])
+                print("file di test complesso " + row['CLIENTE,'], row['LOTTO,'])
+            except:
+                print("keys not found")
